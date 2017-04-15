5996cec10aa38c3e4061335adeeeea1a841cb1d4
Mark Garcia <markwynngarcia@outlook.com>
2017-04-15 12:40:54 +0800

    Add testing for genui.markup header compilation.

---
+++ markup/tests/compilation_test/main.cpp
@@ -0,0 +1,10 @@
+#include "genui/markup/lexer.hpp"
+#include "genui/markup/object_model.hpp"
+#include "genui/markup/parser.hpp"
+#include "genui/markup/source_code.hpp"
+#include "genui/markup/token.hpp"
+
+
+int main() {
+    return 0;
+}
